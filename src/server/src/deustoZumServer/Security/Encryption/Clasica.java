e277871d3dd16e6bd89a0962a29912b5d53fad45
gorkazarate <52033110+gorkazarate@users.noreply.github.com>
2019-10-17 18:02:17 +0200

    Algortimos de Enciptacion y Desencriptacion clasicos acabado

--- src/server/src/deustoZumServer/Security/Encryption/Clasica.java
+++ src/server/src/deustoZumServer/Security/Encryption/Clasica.java
@@ -17,7 +17,21 @@ public class Clasica {
 		return vacio;
 	}
 	
-	
+
+	public static String CesarD(String texto, int codigo) {
+		String vacio="";
+		codigo=codigo %26;
+		for (int i = 0; i < texto.length(); i++) {
+			
+			int c=((int)texto.charAt(i))+97;
+			if (c<97&&c>122) 
+				vacio+=(char) c;
+			vacio+=(char)((c - codigo)%26);
+		}
+		
+		
+		return vacio;
+	}
 	
 	
 	
@@ -30,12 +44,26 @@ public class Clasica {
 			if (c<0&&c>26) 
 				vacio+=(char) c;
 			vacio+=(char)((c + codigo[i%codigoL])%26);
-		
+		}
 		
 		
 		return vacio;
 	}
 	
+		public static String VigenereD(String texto, int[] codigo) {
+			String vacio="";
+			int codigoL = codigo.length;
+			for (int i = 0; i < texto.length(); i++) {
+				
+				int c=((int)texto.charAt(i))+97;
+				if (c<97&&c>122) 
+					vacio+=(char) c;
+				vacio+=(char)((c - codigo[i%codigoL])%26);
+			}
+			
+			
+			return vacio;
+		}
 	
-	
+		
 }
