431db6ca91994d8b718142b81fb7f1fe76e27881
gorkazarate <52033110+gorkazarate@users.noreply.github.com>
2019-11-10 18:22:44 +0100

    Corregir funciones de encriptacion

--- src/server/src/deustoZumServer/Security/EncryptionFactions.java
+++ src/server/src/deustoZumServer/Security/EncryptionFactions.java
@@ -1,49 +1,90 @@
 package deustoZumServer.Security;
 
+import deustoZumServer.Security.Encryption.Clasica;
 import deustoZumServer.Security.Encryption.EncryptionTypes;
 
 public class EncryptionFactions {
-	public void Llamada() {
-		
-	
-	
-	
-	
-		
-	static String getEncrypt(String a, int key, EncryptionTypes e) {
-		switch(e) {
-		
-		case CesarE:
-			
-		return CesarE(a,key);
-	}
-	}
-	case CesarD:
-	public static final String getDecrypt(String a, int key, EncryptionTypes e) {
-	return CesarE(a,key,e) ;
-	}
-	case VigenereE:
-	public static final String getEncrypt(String[] a,  int[] key , EncryptionTypes[] e) {
-	
-	return VigenereE(a,key,e);
-	}
-	case VigenereD:
-	public static final String getDecrypt(String[] a,  int[] key , EncryptionTypes[] e) {
-	return VigenereD(a,key,e);
-	}
-	
-	case RSAE:
-	public static final String getEncrypt(String[] a, int key , EncryptionTypes e) {
-	return RSAE(a,key,e);
-	
-	}
-	case RSAD:
-	public static final String getDecrypt(String[] a, int key , EncryptionTypes e) {
-	return RSAD(a,key,e);
-	}
-	//public static final String getEncrypt(String[] a, int[] key ,EncryptionTypes e) {}
-	//public static final String getDecrypt(String[] a, int[] key ,EncryptionTypes e) {}
-	
+
+
+public static String getEncrypt(String a, int key, EncryptionTypes e) {
+switch(e) {
+case Cesar:
+return Clasica.CesarE(a, key);
+case Vigenere:
+return Clasica.VigenereE(a, new int[] {key});
+default:
+return Clasica.CesarE(a, key);
+}
+
+}
+public static final String getDecrypt(String a, int key, EncryptionTypes e) {
+switch(e) {
+case Cesar:
+return Clasica.CesarD(a, key);
+case Vigenere:
+return Clasica.VigenereD(a, new int[] {key});
+default:
+return Clasica.CesarD(a, key);
 }
 }
-	
+public static final String[] getEncrypt(String[] a,  int[] key , EncryptionTypes[] e) {
+if(a.length!=key.length || key.length != e.length) return null;
+String[] result = new String[a.length];
+for(int i = 0; i < a.length; i++)
+switch(e[i]) {
+case Cesar:
+result[i] = Clasica.CesarE(a[i], key[i]);
+case Vigenere:
+result[i] = Clasica.VigenereE(a[i], new int[] {key[i]});
+default:
+result[i] = Clasica.CesarE(a[i], key[i]);
+}
+return result;
+}
+public static final String[] getDecrypt(String[] a,  int[] key , EncryptionTypes[] e) {
+if(a.length!=key.length || key.length != e.length) return null;
+String[] result = new String[a.length];
+for(int i = 0; i < a.length; i++)
+switch(e[i]) {
+case Cesar:
+result[i] = Clasica.CesarD(a[i], key[i]);
+case Vigenere:
+result[i] = Clasica.VigenereD(a[i], new int[] {key[i]});
+default:
+result[i] = Clasica.CesarD(a[i], key[i]);
+}
+return result;
+}
+
+public static final String[] getEncrypt(String[] a, int[] key ,EncryptionTypes e) {
+if(a.length!=key.length) return null;
+String[] result = new String[a.length];
+for(int i = 0; i < a.length; i++)
+switch(e) {
+case Cesar:
+result[i] = Clasica.CesarD(a[i], key[i]);
+case Vigenere:
+result[i] = Clasica.VigenereD(a[i], new int[] {key[i]});
+default:
+result[i] = Clasica.CesarD(a[i], key[i]);
+}
+return result;
+}
+public static final String[] getDecrypt(String[] a, int[] key ,EncryptionTypes e) {
+if(a.length!=key.length) return null;
+String[] result = new String[a.length];
+for(int i = 0; i < a.length; i++)
+switch(e) {
+case Cesar:
+result[i] = Clasica.CesarD(a[i], key[i]);
+case Vigenere:
+result[i] = Clasica.VigenereD(a[i], new int[] {key[i]});
+default:
+result[i] = Clasica.CesarD(a[i], key[i]);
+}
+return result;
+}
+
+
+
+}
