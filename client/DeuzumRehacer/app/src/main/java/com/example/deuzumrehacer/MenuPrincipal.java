b0dc14c6be1c29f65ea1521ab502591932412e1b
Aritz Zugazaga <aritzugazaga@opendeusto.es>
2019-11-18 19:02:40 +0100

    Actividad Inicio Sesion hecha

---
+++ client/DeuzumRehacer/app/src/main/java/com/example/deuzumrehacer/MenuPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.deuzumrehacer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
