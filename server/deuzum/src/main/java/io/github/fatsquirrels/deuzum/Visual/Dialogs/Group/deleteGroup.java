077bc0c3e5dd436d068de02bb6fb7bc616039aff
Amaia Acha <50750904+amaiaacha@users.noreply.github.com>
2019-12-08 16:15:51 +0100

    comprobación de la información del grupo casi terminado
    falta edit grupo, corregir problema nullpointer en los arrays y pensar como hacer frente a la situacion de que nos introduzca un miembro en el miembro 4 y en el tres no (se perdería información por como estan hechos los arrays)

--- server/deuzum/src/main/java/io/github/fatsquirrels/deuzum/Visual/Dialogs/Group/deleteGroup.java
+++ server/deuzum/src/main/java/io/github/fatsquirrels/deuzum/Visual/Dialogs/Group/deleteGroup.java
@@ -2,8 +2,13 @@ package io.github.fatsquirrels.deuzum.Visual.Dialogs.Group;
 
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
+import io.github.fatsquirrels.deuzum.ServerUserFunctionality;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 
 import javax.swing.JButton;
@@ -15,18 +20,26 @@ public class deleteGroup extends JDialog{
 	private JTextField txtIdGrupo;
 	private JLabel lblIntroducirIdGrupo;
 	private JButton btnEliminar;
+	private Connection conn;
 	
 	/**
 	 * Crea un objeto de deleteGroup, este contiene un Dialogo que permite eliminar un grupo buscandolo 
 	 * a traves de su ID.
 	 */
 	public deleteGroup(Connection c) {
+		this.conn = c;
 		setSize(500,130);
 		setTitle("Eliminar Grupo");
 		setVisible(true);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		getContentPane().setLayout(null);
 		
+		initialize(conn);
+		
+	}
+	
+	public void initialize(Connection c) {
+	
+		getContentPane().setLayout(null);
 		lblIntroducirIdGrupo = new JLabel("Introducir ID Grupo:");
 		lblIntroducirIdGrupo.setBounds(27, 41, 131, 16);
 		getContentPane().add(lblIntroducirIdGrupo);
@@ -39,6 +52,16 @@ public class deleteGroup extends JDialog{
 		btnEliminar = new JButton("Eliminar");
 		btnEliminar.setBounds(338, 36, 117, 29);
 		getContentPane().add(btnEliminar);
+		
+		btnEliminar.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showConfirmDialog(null, "¿Estás seguro de que deseas eliminar el grupo?");
+				ServerUserFunctionality.deleteGroup(c, txtIdGrupo.getText());
+				
+			}
+			
+		});
 	}
 	
 
