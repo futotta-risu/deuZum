2d7be63522e94c0af2f5e2c4064524b5633d51a2
leiregago <leire.gago@opendeusto.es>
2019-12-10 21:19:21 +0100

    Update PanelConfiguracion.java

--- client/desktop/Visual/Paneles/PanelConfiguracion.java
+++ client/desktop/Visual/Paneles/PanelConfiguracion.java
@@ -4,11 +4,13 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.JEditorPane;
 
 public class PanelConfiguracion extends JPanel {
 	private JPasswordField passwordField;
 	private JLabel lblCambiarContrasea = new JLabel();
-	
+	private JPasswordField passwordField_1;
+	private JLabel lblRepetirContrasea = new JLabel();
 	
 	public PanelConfiguracion() {
 		setLayout(null);
@@ -20,6 +22,15 @@ public class PanelConfiguracion extends JPanel {
 		lblCambiarContrasea = new JLabel("Cambiar contrase\u00F1a:");
 		lblCambiarContrasea.setBounds(26, 63, 100, 14);
 		add(lblCambiarContrasea);
+		
+		lblRepetirContrasea = new JLabel("Repetir contrase\u00F1a:");
+		lblRepetirContrasea.setBounds(26, 98, 100, 14);
+		add(lblRepetirContrasea);
+		
+		passwordField_1 = new JPasswordField();
+		passwordField_1.setBounds(191, 95, 109, 20);
+		add(passwordField_1);
+		
 		
 	}
 }
