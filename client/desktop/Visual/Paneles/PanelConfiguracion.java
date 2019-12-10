dd364de0d63f70044dc13b77065f018ffd13d84b
leiregago <leire.gago@opendeusto.es>
2019-12-10 21:10:42 +0100

    panel configuracion y cuentas

--- client/desktop/Visual/Paneles/PanelConfiguracion.java
+++ client/desktop/Visual/Paneles/PanelConfiguracion.java
@@ -1,13 +1,25 @@
 package Paneles;
 
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import javax.swing.JTable;
 
 public class PanelConfiguracion extends JPanel {
-	
+	private JPasswordField passwordField;
+	private JLabel lblCambiarContrasea = new JLabel();
 	
 	
 	public PanelConfiguracion() {
+		setLayout(null);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(191, 60, 109, 20);
+		add(passwordField);
+		
+		lblCambiarContrasea = new JLabel("Cambiar contrase\u00F1a:");
+		lblCambiarContrasea.setBounds(26, 63, 100, 14);
+		add(lblCambiarContrasea);
 		
 	}
-
 }
--- client/desktop/Visual/Paneles/PanelCuentas.java
+++ client/desktop/Visual/Paneles/PanelCuentas.java
@@ -1,12 +1,25 @@
 package Paneles;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JList;
 
 public class PanelCuentas extends JPanel {
 	
-	
+	private static final long serialVersionUID = 1L;
+	private JLabel lblEstasSonMis = new JLabel();
+	private JList listCuentas = new JList();
 	
 	public PanelCuentas() {
+		setLayout(null);
+		
+		lblEstasSonMis = new JLabel("Estas son mis cuentas:");
+		lblEstasSonMis.setBounds(30, 48, 109, 14);
+		add(lblEstasSonMis);
+		
+		listCuentas = new JList();
+		listCuentas.setBounds(175, 47, 251, 113);
+		add(listCuentas);
 		
 	}
 }
