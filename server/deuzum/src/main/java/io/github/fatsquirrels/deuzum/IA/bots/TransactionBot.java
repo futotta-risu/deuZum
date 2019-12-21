9a6cb6d7e4d6682218189dd5b7eb4b37a49a5f87
landersanmi <50888543+landersanmi@users.noreply.github.com>
2019-12-21 12:53:36 +0100

    Terminados TransactionBot y UserBot

--- server/deuzum/src/main/java/io/github/fatsquirrels/deuzum/IA/bots/TransactionBot.java
+++ server/deuzum/src/main/java/io/github/fatsquirrels/deuzum/IA/bots/TransactionBot.java
@@ -3,6 +3,7 @@ package io.github.fatsquirrels.deuzum.IA.bots;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
 
 import io.github.fatsquirrels.deuzum.database.GeneralSQLFunctions;
 
@@ -11,11 +12,13 @@ public class TransactionBot extends BotBase{
 	private String name;
 	private Thread hiloTransactions;
 	private Connection conn;
+	private long cantidad;
 	
 	
-	public TransactionBot(String name){
+	public TransactionBot(String name, long cantidad){
 		this.name = name;
 		this.conn = GeneralSQLFunctions.connectToDatabase("jdbc:mysql://localhost:3306/deuzumdb", "root", "");
+		this.cantidad = cantidad;
 	}
 	
 
@@ -25,31 +28,32 @@ public class TransactionBot extends BotBase{
 
 			public void run() {
 				int users = 0;
+				String[] arrIds = null;
+				
 				try {
 					ResultSet ids = GeneralSQLFunctions.getExecQuery(conn, "SELECT id FROM usuario");
 					ResultSet userCount = GeneralSQLFunctions.getExecQuery(conn, "SELECT count(id) FROM usuario");
 					if(userCount.next()) {
-					users = userCount.getInt("count(id)");
-					
+						users = userCount.getInt("count(id)");
+						arrIds = new String[users];
+						userCount.close();
 					}
-					//TODO ACCEDER AL RESULTSET DE MANERA ALEATORIA
 					//(int)Math.random()*(users-1)+1);
+					int counter = 0;
 					while(ids.next()){
-						String idFrom = ids.getString("id");
-						String idTo = "";
-						if (ids.next()) {
-							idTo = ids.getString("id");
-							int random = (int) Math.random()*(100-0)+0;
-							String StrRandom = Integer.toString(random);
-							GeneralSQLFunctions.insertEntryIntoDatabase(conn, "transaccion", new String[] {"source", "destino", "dinero"}, 
-									new String[] {idFrom, idTo, StrRandom});
-						}
-						ids.close();
-						//ids = GeneralSQLFunctions.getExecQuery(conn, "SELECT id FROM usuario");
+						String id = ids.getString("id");
+						arrIds[counter] = id;
+						counter++;
+					}
+					for (int i = 0; i < cantidad; i++) {
+						Random r = new Random(i);
+						String randomId1 = arrIds[r.nextInt(users)];
+						String randomId2 = arrIds[r.nextInt(users)];
+						String cantidad = Integer.toString(r.nextInt(9999));
+					GeneralSQLFunctions.insertEntryIntoDatabase(conn, "transaccion", new String[] {"source", "destino","dinero"},
+					new String[] {randomId1,randomId2, cantidad});
+					}
 						
-	
-						}
-					
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
@@ -72,8 +76,9 @@ public class TransactionBot extends BotBase{
 		hiloTransactions.interrupt();
 	}
 	
-	public static void main(String[] args) {
-		TransactionBot tb1 = new TransactionBot("TransactionBot1");
+	/*public static void main(String[] args) {
+		TransactionBot tb1 = new TransactionBot("TransactionBot1", 10000);
 		tb1.execute();
 	}
+	*/
 }
